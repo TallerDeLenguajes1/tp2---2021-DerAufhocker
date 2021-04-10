c08583697f58e02a776adf69841343384ac491b1
Exequiel496 <diazacademy628@gmail.com>
2021-04-10 00:59:33 -0300

    mi primer commit

---
+++ tp2_1_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 4
+#define M 5
+
+int main(){
+    
+    int f,c;
+    double mt[N][M];
+
+    for ( f = 0; f < N; f++)
+    {
+        printf ("%s", mt[N][M]);
+    }
+    printf("\n");
+
+    system ("pause");
+    return 0;
+}
